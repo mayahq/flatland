1eea0d2ca4e435c357a74407b24254f78b9f4121
ahgamut <41098605+ahgamut@users.noreply.github.com>
2021-10-07 16:56:17 +0530

    modified generator output

--- src/flatland/augment/generators/circles_only.py
+++ src/flatland/augment/generators/circles_only.py
@@ -4,12 +4,15 @@ from collections import namedtuple
 
 CircleParams = namedtuple("CircleParams", ["x", "y", "r"])
 
+
 def generate(N, xmin=-96, xmax=96, ymin=-96, ymax=96, rmin=3, rmax=50):
-    return [
-        CircleParams(
-            random.randint(xmin, xmax),
-            random.randint(ymin, ymax),
-            random.randint(rmin, rmax),
-        )
-        for i in range(N)
-    ]
+    return {
+        "items": [
+            CircleParams(
+                random.randint(xmin, xmax),
+                random.randint(ymin, ymax),
+                random.randint(rmin, rmax),
+            )
+            for i in range(N)
+        ]
+    }
--- src/flatland/augment/generators/rotating_line1.py
+++ src/flatland/augment/generators/rotating_line1.py
@@ -5,14 +5,12 @@ from collections import namedtuple
 TurnMove1Params = namedtuple("TurnMove1Params", ["start", "N", "step", "theta"])
 
 
-
-def get_rotmove1(N, xmin=-48, xmax=48, ymin=-48, ymax=48):
-    return TurnMove1Params(
-        start=(random.randint(xmin, xmax), random.randint(ymin, ymax)),
-        N=random.randint(20, 96),
-        step=random.randint(20, 96),
-        theta=random.randint(0, 181),
-    )
-
-
-
+def generate(N, xmin=-48, xmax=48, ymin=-48, ymax=48):
+    return {
+        "items": TurnMove1Params(
+            start=(random.randint(xmin, xmax), random.randint(ymin, ymax)),
+            N=N,
+            step=random.randint(20, 96),
+            theta=random.randint(0, 181),
+        )
+    }
