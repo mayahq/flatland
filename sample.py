3c409bceb2366c2e3e222d6afa55614a4383b2ec
ahgamut <41098605+ahgamut@users.noreply.github.com>
2021-10-07 16:51:33 +0530

    modified sample

--- sample.py
+++ sample.py
@@ -1,5 +1,9 @@
-from flatland.utils.primitives import Line, Circle, Rectangle, Stop
-from flatland.utils.modding import initialize, finalize
+from flatland.utils.modding import finalize
+from flatland.utils.modding import initialize
+from flatland.utils.primitives import Circle
+from flatland.utils.primitives import Line
+from flatland.utils.primitives import Rectangle
+from flatland.utils.primitives import Stop
 
 initialize()
 
@@ -7,7 +11,7 @@ initialize()
 
 Circle((0, 0), 10)
 Line((0, 0), (50, 50))
-Rectangle((20, 20), 15, 22, 20)
+Rectangle((20, 20), l=15, b=22, theta=20)
 Stop()
 
 # <> Program END
