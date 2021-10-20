ce5f11beb30a3e0b3229dd2c23d05c16b3a49758
Gautham <41098605+ahgamut@users.noreply.github.com>
2021-10-20 23:46:38 +0000

    recursive fix

--- src/flatland/metrics/distances/recursive.py
+++ src/flatland/metrics/distances/recursive.py
@@ -4,8 +4,12 @@ import numpy as np
 
 def compare_subparts(part1, part2):
     if isinstance(part1, (int, float)):
-        # 256 because image size is 256x256
-        answer = 1 - abs(part1 - part2) / 256
+        a = max([part1, part2])
+        b = min([part1, part2])
+        if abs(a - b) < 1e-2:
+            answer = 1
+        else:
+            answer = b / a
     elif isinstance(part1, str):
         answer = float(part1 == part2)
     elif isinstance(part1, list):
@@ -23,7 +27,7 @@ def compare_subparts(part1, part2):
     return np.round(answer, 2)
 
 
-def compare_nodes(node1, node2):
+def compare_circles(node1, node2):
     num = 0
     den = 0
     for k in node1.keys():
@@ -36,9 +40,36 @@ def compare_nodes(node1, node2):
     return wt
 
 
+def compare_lines(node1, node2):
+    num = 0
+    den = 0
+    for k in node1.keys():
+        if k in ("id", "start", "end"):
+            continue
+        a = compare_subparts(node1[k], node2[k])
+        num += a
+        den += 1
+    num += max(
+        [
+            compare_subparts(node1["start"], node2["start"])
+            + compare_subparts(node1["end"], node2["end"]),
+            compare_subparts(node1["start"], node2["end"])
+            + compare_subparts(node1["end"], node2["start"]),
+        ]
+    )
+    den += 2
+    wt = num / den
+    return wt
+
+
 def node_weighter(node1, node2):
     if node1["type"] == node2["type"]:
-        return compare_nodes(node1, node2)
+        if node1["type"] == "circle":
+            return compare_circles(node1, node2)
+        elif node1["type"] == "line":
+            return compare_lines(node1, node2)
+        else:
+            return 0
     else:
         return 0
 
