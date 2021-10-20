c1c8e740e44c83ad3ccab90d38a1edacafd1b68b
ahgamut <41098605+ahgamut@users.noreply.github.com>
2021-10-20 23:31:32 +0530

    added circle in box template

--- src/flatland/augment/generators/__init__.py
+++ src/flatland/augment/generators/__init__.py
@@ -1,3 +1,4 @@
+from .circle_in_box import generate as get_circle_in_box
 from .circles_only import generate as get_circles
 from .lines_only import generate as get_lines
 from .rectangles_only import generate as get_rectangles
@@ -11,6 +12,7 @@ TEMPLATE_MAP = {
     "rectangles": "rectangles_only.py.jinja",
     "rotline1": "rotating_line1.py.jinja",
     "stickfigure1": "stickfigure1.py.jinja",
+    "circle_in_box": "circle_in_box.py.jinja",
 }
 FUNC_MAP = {
     "circles": get_circles,
@@ -18,4 +20,5 @@ FUNC_MAP = {
     "rectangles": get_rectangles,
     "rotline1": get_rotline1,
     "stickfigure1": get_stickfigure1,
+    "circle_in_box": get_circle_in_box,
 }
