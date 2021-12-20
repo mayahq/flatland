ca9233ae53c3afd95c5c66691b249fee7191c643
ahgamut <41098605+ahgamut@users.noreply.github.com>
2021-12-20 13:19:16 +0530

    library stores file locations
    to later implement randomization

--- src/flatland/library/base.py
+++ src/flatland/library/base.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import os
+import random
 from dataclasses import asdict
 from dataclasses import dataclass
 from typing import Dict
@@ -8,10 +9,12 @@ from typing import List
 from typing import Optional
 
 import flatland.utils.config as CONFIG
+from flatland.augment import single_file
 from flatland.lang.primitives import Flow
 from flatland.lang.primitives import Node
 from flatland.lang.primitives import standard_env
 from flatland.lang.run import main as runner
+from flatland.utils.misc import check_dir
 
 
 @dataclass
@@ -23,8 +26,12 @@ class Primitive:
 
 
 class Library:
-    def __init__(self):
-        self.existing = []
+    def __init__(self, directory):
+        directory = check_dir(directory)
+        self.files = glob.glob(os.path.join(directory, "*.fbp")) + glob.glob(
+            os.path.join(directory, "*.lisp")
+        )
+        self.existing_primitives = []
         self.num_primitives = 0
         self.primitives = dict()
         self.connections = dict()
@@ -35,10 +42,10 @@ class Library:
         else:
             tp = node.tp
 
-        if tp in self.existing:
+        if tp in self.existing_primitives:
             return tp
 
-        self.existing.append(tp)
+        self.existing_primitives.append(tp)
         self.primitives[self.num_primitives] = Primitive(**node.__random_details__())
         self.connections[self.num_primitives] = set()
         self.num_primitives += 1
@@ -52,8 +59,8 @@ class Library:
         return tp
 
     def add_connection(self, tp1, tp2):
-        ind1 = self.existing.index(tp1)
-        ind2 = self.existing.index(tp2)
+        ind1 = self.existing_primitives.index(tp1)
+        ind2 = self.existing_primitives.index(tp2)
         self.connections[ind1].add(ind2)
 
     def to_dict(self):
@@ -64,6 +71,28 @@ class Library:
 
     def __repr__(self):
         return json.dumps(self, indent=4, cls=Encoder)
+
+    def fill_dependencies(self):
+        env = standard_env()
+        CONFIG.RANDOMIZE = False
+        CONFIG.RUN = False
+        for file in self.files:
+            with open(file) as f:
+                prog = f.read()
+            runner(prog, file, env=env)
+
+        for v in env.values():
+            if isinstance(v, Node):
+                self.add_primitive(v)
+
+    def _generate_programs(self, N, output_dir):
+        rd, run = CONFIG.RANDOMIZE, CONFIG.RUN
+        for i in range(N):
+            file = random.choice(self.files)
+            with open(file) as f:
+                prog = f.read()
+            single_file(prog, file, 1, output_dir)
+        CONFIG.RANDOMIZE, CONFIG.RUN = rd, run
 
 
 class Encoder(json.JSONEncoder):
@@ -78,21 +107,6 @@ class Encoder(json.JSONEncoder):
 
 
 def create_library(directory):
-    lib = Library()
-    files = glob.glob(os.path.join(directory, "*.fbp")) + glob.glob(
-        os.path.join(directory, "*.lisp")
-    )
-
-    env = standard_env()
-    CONFIG.RANDOMIZE = False
-    CONFIG.RUN = False
-    for file in files:
-        with open(file) as f:
-            prog = f.read()
-        runner(prog, file, env=env)
-
-    for v in env.values():
-        if isinstance(v, Node):
-            lib.add_primitive(v)
-
+    lib = Library(directory)
+    lib.fill_dependencies()
     return lib
